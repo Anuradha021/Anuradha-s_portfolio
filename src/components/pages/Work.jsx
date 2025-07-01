de9528d303ca92abb37d5ab63fa75ba4255b3b49
Anuradha021 <logicalanuradha@gmail.com>
2025-07-01 23:40:33 +0530

    work page updated

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -4,13 +4,14 @@ import split from "../../assets/split.png";
 // Replace with your image
 import ProjectCard from "../ProjectCard";
 import movie_store from "../../assets/movie_store.png"
+import travel_bucket from "../../assets/travel_bucket.png"
 const projects = [
   {
     number: 1,
-    title: "Spli Expense  ",
+    title: "Split Expense  ",
     desc:
       "The Split Expense App is designed to help groups of friends, roommates, or teams manage shared expenses efficiently. Users can create or join groups, add expenses, and track who paid what. The app automatically calculates each member's balance and shows how much they owe or are owed. Users must be authenticated to access or contribute to any group, ensuring secure and private data handling. The interface is responsive and easy to use across all devices, making expense tracking simple and intuitive for users on the go.",
-    tech: "ReactJS, Node,Express, TailwindCSS",
+    tech: "ReactJS, Node,Express, TailwindCSS,MongoDB",
     image: split,
     liveLink: "https://your-live-demo-link.com",
     githubLink: "https://github.com/Anuradha021/split_expense",
@@ -20,10 +21,20 @@ const projects = [
     title: "Movie Store ",
     desc:
       "This a Web app having interactive UI, with attractive user interface where you can search and see ratings and descriptions of your favourite movie. All It to movies are fetched using IMDB Api and you can also provide your rating into it. is basically a Frontend project which | have created to learn to handle data fetching from API’s and also handle side effects, prop drilling, component creation in effective way so to improve code readability.",
-    tech: "ReactJS, Node,Express, TailwindCSS",
+    tech: "ReactJS, Node,Express, TailwindCSS,IMDB API",
     image: movie_store,
     liveLink: "https://your-live-demo-link.com",
     githubLink: "https://github.com/Anuradha021/Your_movie_store",
+  },
+  {
+    number: 2,
+    title: "Travel Bucket",
+    desc:
+      "Sometimes out of excitement of going on a trip, we forget the essential part of a trip that is planning. Here is the List where you can plan your trip as a checklist. Things you should take with you and tasks which you have to do. Mark them if added or completed so that no regret is left after reaching your destination. Arrange your list, add the quantity of items, and Sort them based on their names and order of addition to the list. ",
+    tech: "ReactJS, Node,Express, TailwindCSS,LocalStorage",
+    image: travel_bucket,
+    liveLink: "https://travellist21.netlify.app/",
+    githubLink: "https://github.com/Anuradha021/Travel_Bucket",
   },
   
   // Add more project objects here
